8d8df04954e72ceec04c47a5db4d6f163bcac010
Gian <gian.aguilar.93@gmail.com>
2023-11-27 00:51:21 -0600

    newest
    aha

--- processing.R
+++ processing.R
@@ -63,3 +63,86 @@ listing2 |>
   filter(is.na(age)) |> select(interview__key)
 
 
+gtcars |>
+  dplyr::select(model, year, hp, trq) |>
+  dplyr::slice(1:8) |>
+  gt(rowname_col = "model") |>
+  tab_row_group(
+    label = "numbered",
+    rows = matches("^[0-9]")
+  )
+
+anal_vars <- c('strata', 'age_group', 'ethnicity', 'education', 'workertype_group')
+
+households |> 
+  select(all_of(anal_vars), starts_with('k4')) |> 
+  select(-k4_1) |> 
+  pivot_longer(
+    cols = starts_with('k4'),
+    names_to = 'k4_item',
+    values_to = 'k4_value'
+  ) |> 
+  filter(k4_value == 1) |> 
+  tabyl(strata, k4_item) |> 
+  adorn_totals(c('col'))
+
+households |> 
+  select(all_of(anal_vars), starts_with('k4')) |> 
+  select(-k4_1) |> 
+  pivot_longer(
+    cols = starts_with('k4'),
+    names_to = 'item',
+    values_to = 'value'
+  ) |> 
+  group_by(strata, item) |> 
+  summarise(
+    selected = sum(value == 1, na.rm = T),
+    total = sum(!is.na(value), na.rm = T),
+    prop = selected/total,
+    pct = paste0(round(prop * 100, 1), '%'),
+    full = paste0(pct, '(', selected,')')
+  ) |> 
+  select(strata, item, full) |> 
+  pivot_wider(
+    names_from = item,
+    values_from = full
+  )
+
+multi_tabulate <- function(row, col){
+  
+  households |> 
+    select(all_of(anal_vars), starts_with(col)) |> 
+    pivot_longer(
+      cols = starts_with(col),
+      names_to = 'item',
+      values_to = 'value'
+    ) |> 
+    group_by(!!as.name(row), item) |> 
+    summarise(
+      selected = sum(value == 1, na.rm = T),
+      total = sum(!is.na(value), na.rm = T),
+      prop = selected/total,
+      pct = paste0(round(prop * 100, 1), '%'),
+      full = paste0(pct, '(', selected,')')
+    ) |> 
+    select(!!as.name(row), item, full) |> 
+    pivot_wider(
+      names_from = item,
+      values_from = full
+    )
+  
+}
+
+
+multi_aggregate <- function(var){
+  df_list <- purrr::map(anal_vars, ~multi_func1(.x, var))
+  
+  rbind(df_list[[1]] |> rename(Item = strata),
+        df_list[[2]] |> rename(Item = age_group),
+        df_list[[3]] |> rename(Item = ethnicity),
+        df_list[[4]] |> rename(Item = education),
+        df_list[[5]] |> rename(Item = workertype_group)
+  )
+}
+
+
